8267a04026a888dad7db139c157468356ad15b0f
gabrieljudesolomon <124865641+gabrieljudesolomon@users.noreply.github.com>
2023-02-14 11:33:34 +0000

    Initial commit

--- factorial.py
+++ factorial.py
@@ -8,3 +8,7 @@ else:
    for i in range(1,num + 1):    
        factorial = factorial*i    
    print("The factorial of",num,"is",factorial)    
+if else:    
+   for i in range(1,num + 1):    
+       factorial = factorial*i    
+   print("The factorial of",num,"is",factorial)    
